d9fd627894d33e789bece392855d41a3e9793cef
Nicolas Veliz <niveliz@miuandes.cl>
2020-03-29 22:25:43 -0300

    parte 1.1

---
+++ laboratorio2/laboratorio2/Cancion.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laboratorio2
+{
+    public class Cancion
+    {
+
+        public string nombre;
+        public string artista;
+        public string album;
+        public string genero;
+
+        public Cancion(string nombre, string artista, string album, string genero)
+        {
+            this.nombre = nombre;
+            this.artista = artista;
+            this.album = album;
+            this.genero = genero;
+        }
+        public string Informacion()
+        {
+
+            return ("Nombre: "+ nombre + ", Artista: "+ artista + ", Album: "+ album + ", Genero: "+ genero);
+            
+        }
+        
+        
+        
+          
+        
+    }
+}
+
--- laboratorio2/laboratorio2/Program.cs
+++ laboratorio2/laboratorio2/Program.cs
@@ -6,10 +6,20 @@ using System.Threading.Tasks;
 
 namespace laboratorio2
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            Cancion cancion1 = new Cancion("Entre canibales", "Soda", "Comfort y musica para volar", "rock");
+            Cancion cancion2 = new Cancion("True love", "SOJA", "Peace in time of war", "Regae");
+            Cancion cancion3 = new Cancion("Ignorantes", "Bad bunny", "YHLQMDLG", "Trap");
+
+            Console.WriteLine(cancion1.Informacion());
+            Console.WriteLine(cancion2.Informacion());
+            Console.WriteLine(cancion3.Informacion());
+
+            Console.ReadKey();
+
         }
     }
 }
